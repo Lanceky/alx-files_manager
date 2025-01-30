fe16e3ebd6f0fe79064ea31309f7679ddc3b16ca
Predator <evanslance100@gmail.com>
2025-01-30 23:44:12 +0300

    feat(routes): define all API endpoints for status, users, auth, and files operations

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-const express = require('express');
-const FilesController = require('../controllers/FilesController');
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -16,5 +15,8 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
