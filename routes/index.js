439fcbfd6c79a9087d9c9d4b652040c634a0cc4a
Predator <evanslance100@gmail.com>
2025-02-04 16:51:40 +0300

    define all API endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 
-module.exports = router;
+export default router;
